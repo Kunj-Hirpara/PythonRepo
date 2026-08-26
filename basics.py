18eb049d0619de8173772160aa8c96dfeb54d43f
Kunj Hirpara <kunjhirapara61@gmail.com>
2026-08-26 22:31:02 +0530

    Basic of Python

---
+++ basics.py
@@ -0,0 +1,9 @@
+import math
+import random
+
+x = (random.randrange(1,50))
+y = (random.randrange(1,50))
+
+print("Square Root of number: ", math.sqrt(x))
+print("Cosine of number: ", math.cos(y))
+print("number :", math.pow(x,y))
