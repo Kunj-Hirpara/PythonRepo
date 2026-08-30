7bb358d8a365d7789d47e95fd27f2e9bb1c802a0
Kunj Hirpara <kunjhirapara61@gmail.com>
2026-08-30 19:26:08 +0530

    Functions

--- basic_function.py
+++ basic_function.py
@@ -40,3 +40,48 @@
 # b = int(input("Enter Number 2 : "))
 # opt = input("Enter the sign (+,-,*,/) : ")
 # op(a, b, opt)
+
+# *args Function
+
+# def s_d(name, rno, *marks):
+#     print("Student Name: ", name)
+#     print("Student RollNo: ", rno)
+#     print("Marks: ", marks)
+
+#     total = sum(marks)
+#     print("Total: ", total)
+
+#     per = (total/ (len(marks) * 100)) * 100
+#     print("Percentage: ", per)
+
+# name = input("Enter the name: ")
+# rno = int(input("Enter the RollNo: "))
+# marks = list(map(int, input("Enter the marks: ").split()))
+# s_d(name,rno,*marks)
+
+
+# use lambda function'
+# In this function is use the only one line function
+
+# add = lambda x,y : x+y
+# a = int(input("Enter a: "))
+# b = int(input("Enter b: "))
+# print("Addition: ", add(a,b))
+
+list1 = list(map(int, input("Enter the list1: ").split()))
+list2 = list(map(int, input("Enter the list2: ").split()))
+print("List1: ", list1)
+print("List2: ", list2)
+fun = list(map(lambda x,y : x*y, list1, list2))
+print(fun)
+
+
+# Global Function
+
+# n = 10
+# def check():
+#     global n
+#     n += 10
+#     print("Inside n = ", n)
+# check()
+# print("Outside n = ", n)
