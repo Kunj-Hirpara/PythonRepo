fccfe30e83a18b548d9da4adeb4b83bc9a197fa7
Kunj Hirpara <kunjhirapara61@gmail.com>
2026-08-31 23:21:17 +0530

    Class Inheritance

---
+++ basic_all_inheritance.py
@@ -0,0 +1,33 @@
+# Single Inheritance
+
+# class parent:
+#     print("Hello Parent.")
+
+# class child(parent):
+#     def __init__(self):
+#         super().__init__() #Optional Line
+#         print("Hello Child.")
+# c1 = child()
+
+
+# Multiple Inheritance
+
+class c1:
+    def getname(self,name):
+        self.name = name
+
+class c2:
+    def getmarks(self,marks):
+        self.marks = marks
+
+class c3(c1,c2):
+    def disp(self):
+        print("Student Name: ", self.name)
+        print("Student Marks: ", self.marks)
+
+name = input("Enter student name: ")
+marks = int(input("Enter student marks: "))
+obj = c3()
+obj.getname(name)
+obj.getmarks(marks)
+obj.disp()
