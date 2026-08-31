73b9c744e60487fc016edf366e1cb2175aa45e67
Kunj Hirpara <kunjhirapara61@gmail.com>
2026-08-31 23:28:26 +0530

    Class Inheritance

--- basic_all_inheritance.py
+++ basic_all_inheritance.py
@@ -12,21 +12,44 @@
 
 # Multiple Inheritance
 
+# class c1:
+#     def getname(self,name):
+#         self.name = name
+
+# class c2:
+#     def getmarks(self,marks):
+#         self.marks = marks
+
+# class c3(c1,c2):
+#     def disp(self):
+#         print("Student Name: ", self.name)
+#         print("Student Marks: ", self.marks)
+
+# name = input("Enter student name: ")
+# marks = int(input("Enter student marks: "))
+# obj = c3()
+# obj.getname(name)
+# obj.getmarks(marks)
+# obj.disp()
+
+
+# Multi-Level Inheritance
+
 class c1:
-    def getname(self,name):
+    def getname(self, name):
         self.name = name
 
-class c2:
-    def getmarks(self,marks):
+class c2(c1):
+    def getmarks(self, marks):
         self.marks = marks
 
-class c3(c1,c2):
+class c3(c2):
     def disp(self):
-        print("Student Name: ", self.name)
-        print("Student Marks: ", self.marks)
+        print("Name :", self.name)
+        print("Marks :", self.marks)
 
-name = input("Enter student name: ")
-marks = int(input("Enter student marks: "))
+name = input("Enter the name :")
+marks = int(input("Enter the marks :"))
 obj = c3()
 obj.getname(name)
 obj.getmarks(marks)
