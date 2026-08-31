12bb026b95e9c006e1f982f83407a490565404d4
Kunj Hirpara <kunjhirapara61@gmail.com>
2026-08-31 23:46:00 +0530

    Basic Exception Handling

---
+++ basic_exception.py
@@ -0,0 +1,41 @@
+# try:
+#     print(x)
+# except:
+#     print("An exception occurred.")
+# finally:
+#     print("In Finally code.")
+
+
+# try:
+#     print("Hello")
+# except:
+#     print("An exception occurred.")
+# else:
+#     print("Nothing went wrong.")
+# finally:
+#     print("In Finally code.")
+
+
+# Built-in Exception
+
+# try:
+#     k = 5/0
+#     print(k)
+# except ZeroDivisionError:
+#     print("can't divide by zero.")
+# finally:
+#     print("Finally is always executed.")
+
+
+# a = [1,2,3]
+# try:
+#     print("Second Element: ", a[1])
+#     print("Fourth Element: ", a[3])
+# except IndexError:
+#     print("An error occurred.")
+
+
+try:
+    print(x)
+except NameError:
+    print("An Exception Occurred.")
