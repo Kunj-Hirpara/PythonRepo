6c4e2eb786a7daf691e80ab928ad41c973224d49
Kunj Hirpara <kunjhirapara61@gmail.com>
2026-09-06 14:24:22 +0530

    All type of exception handling

--- basic_exception.py
+++ basic_exception.py
@@ -35,7 +35,52 @@
 #     print("An error occurred.")
 
 
-try:
-    print(x)
-except NameError:
-    print("An Exception Occurred.")
+# try:
+#     print(x)
+# except NameError:
+#     print("An Exception Occurred.")
+# finally:
+#     print("In Finally code.")
+
+
+# Raising Exception
+
+# x = int(input("Enter an integer value: "))
+# try:
+#     if x < 0:
+#         raise Exception("Sorry, no numbers below zero")
+#     else:
+#         print(x)
+# except Exception:
+#     print("Error Occurred.")
+
+
+# user define exception
+
+# class InvalidAgeException(Exception):
+#     print("Raised when the input value is less than 18.")
+#     pass
+# try:
+#     age = int(input("Enter your age: "))
+#     if age < 18:
+#         raise InvalidAgeException
+#     else:
+#         print("You are eligible to vote.")
+# except InvalidAgeException:
+#     print("Sorry, you are not eligible to vote.")
+
+
+# Assert Keyword
+
+# x = "Hello"
+# assert x == "Hello"
+
+# a = 4
+# b = 2
+# print("The Value of a/b is : ")
+# assert b != 0
+# print(a/b)
+
+x = int(input("Enter an integer value: "))
+assert x > 0
+print(x)
