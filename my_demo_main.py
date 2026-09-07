c61531c1da52dcfbbf2cceb8ae402042336d8eaf
Kunj Hirpara <kunjhirapara61@gmail.com>
2026-09-07 18:42:14 +0530

    Module inside the folder and main file is outside the folder

---
+++ my_demo_main.py
@@ -0,0 +1,9 @@
+from my_demo import add, sub, mul, div
+
+a = int(input("Enter first number: "))
+b = int(input("Enter second number: "))
+
+print("Addition: ", add.add(a,b))
+print("Subtraction: ", sub.sub(a,b))
+print("Multiplication: ", mul.mul(a,b))
+print("Division: ", div.div(a,b))
