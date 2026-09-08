f2188681715980f5ee8b4faafabd9e5868e48333
Kunj Hirpara <kunjhirapara61@gmail.com>
2026-09-08 19:49:32 +0530

    Basic Iterator

---
+++ basic_iterator.py
@@ -0,0 +1,8 @@
+# What is an Iterator?
+# An iterator is an object that allows you to access elements one at a time.
+
+numbers = [10, 20, 30]
+iterator = iter(numbers)
+print(next(iterator))
+print(next(iterator))
+print(next(iterator))
