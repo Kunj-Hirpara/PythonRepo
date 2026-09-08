1b5fbce8d898edb29d621724bdace7b88c9bd2d4
Kunj Hirpara <kunjhirapara61@gmail.com>
2026-09-08 19:59:28 +0530

    Basic Iterator

--- basic_iterator.py
+++ basic_iterator.py
@@ -6,3 +6,48 @@ iterator = iter(numbers)
 print(next(iterator))
 print(next(iterator))
 print(next(iterator))
+
+names = ["abc", "xyz", "pqr"]
+it = iter(names)
+print(next(it))
+print(next(it))
+print(next(it))
+
+
+num = [1, 2, 3]
+it = iter(num)
+for i in it:
+    print(i)
+
+
+name = "Kunj"
+it = iter(name)
+print(next(it))
+print(next(it))
+print(next(it))
+print(next(it))
+
+
+class Count:
+
+    def __init__(self, max_value):
+        self.max_value = max_value
+        self.current = 1
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+
+        if self.current <= self.max_value:
+            value = self.current
+            self.current += 1
+            return value
+
+        raise StopIteration
+
+
+numbers = Count(5)
+
+for number in numbers:
+    print(number)
