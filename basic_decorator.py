18d8153a1a16c82b28e40e5817b311b0dbd9f929
Kunj Hirpara <kunjhirapara61@gmail.com>
2026-09-09 22:34:32 +0530

    Basic Decorator

---
+++ basic_decorator.py
@@ -0,0 +1,12 @@
+def decorator(func):
+    def wrapper():
+        print("Before the function call.")
+        func()
+        print("After the function call.")
+    return wrapper
+
+@decorator
+def hello():
+    print("Hello world!")
+
+hello()
