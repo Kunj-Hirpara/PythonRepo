dff1b0225d8d8f3e4c1ad2c99c32465aa8a8cc3e
Kunj Hirpara <kunjhirapara61@gmail.com>
2026-09-09 22:56:07 +0530

    Basic Decorator

--- basic_decorator.py
+++ basic_decorator.py
@@ -10,3 +10,33 @@ def hello():
     print("Hello world!")
 
 hello()
+
+# Decorator with arguments
+def decorator(func):
+    def wrapper(name):
+        print("Before")
+        func(name)
+        print("After")
+    return wrapper
+
+@decorator
+def hello(name):
+    print("Hello ", name)
+hello("world!")
+
+# *args and **kwargs
+def decorator(func):
+    def wrapper(*args, **kwargs):
+        print("Function started")
+        result = func(*args, **kwargs)
+        print("Function ended")
+        return result
+    return wrapper
+
+@decorator
+def add(a, b):
+    return a + b
+
+answer = add(10, 20)
+print("Result:", answer)
+
